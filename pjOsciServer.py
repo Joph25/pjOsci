3659a8e79bb3940bcdc995ce71840d9a6fa7269f
Philipp@RasPI3 <philipp.jonas@gmx.at>
2017-01-04 20:40:23 +0100

    second lines

--- pjOsciServer.py
+++ pjOsciServer.py
@@ -1,6 +1,10 @@
 import visa 
 
-rm = visa.ResourceManager('@py')
-rm.list_resources()
+#rm = visa.ResourceManager('@py')
+#rm.list_resources()
 #inst = rm.open_resource('')
 #print(inst.query("*IDN?"))
+
+for i in range(7):
+	print i
+

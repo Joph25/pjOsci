c977bb0a2071d6a0063921d773ed328dac9ab548
Unknown <philipp.jonas@gmx.at>
2017-01-10 15:11:41 +0100

    Sim Updates

--- pjOsciServer.py
+++ pjOsciServer.py
@@ -1,12 +1,9 @@
-#import visa 
+import visa 
 
-#rm = visa.ResourceManager('@py')
-#rm.list_resources()
+rm = visa.ResourceManager('@py')
+rm.list_resources()
 #inst = rm.open_resource('')
 #print(inst.query("*IDN?"))
 
-for i in range(7):
-	print( i)
-for j in range (10):
-	print("j is:"+str(j))
+
     

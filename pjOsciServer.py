b60a59c6ccd5366a11120e7a80d4094dc478039f
Unknown <philipp.jonas@gmx.at>
2017-01-10 16:24:50 +0100

    OsciServer Comments

--- pjOsciServer.py
+++ pjOsciServer.py
@@ -1,9 +1,9 @@
 import visa 
 
-rm = visa.ResourceManager('@py')
-rm.list_resources()
-#inst = rm.open_resource('')
-#print(inst.query("*IDN?"))
+rm = visa.ResourceManager('@py')                      #opens the resource manager for the Raspberry Pi
+rm.list_resources()                                                 #lists all available connections on your device
+inst = rm.open_resource('<your resource>')     #opens your connection
+print(inst.query("*IDN?"))                                    #returns the identification of your device
 
 
     
